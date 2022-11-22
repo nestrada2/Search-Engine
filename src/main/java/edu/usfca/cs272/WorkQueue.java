f707a49af6cfc08224aab4f1ed1259d99d50594e
nestrada2 <81432763+nestrada2@users.noreply.github.com>
2022-11-22 14:39:00 -0800

    remove print statement in WorkQueue

--- src/main/java/edu/usfca/cs272/WorkQueue.java
+++ src/main/java/edu/usfca/cs272/WorkQueue.java
@@ -202,7 +202,6 @@ public class WorkQueue {
 
 		@Override
 		public void run() {
-			System.out.println("Worker thread starting...");
 			Runnable task = null;
 
 			try {
@@ -249,63 +248,5 @@ public class WorkQueue {
 
 			log.debug("Worker thread terminating...");
 		}
-	}
-
-	// TODO Remove main method when done testing!
-
-	/**
-	 * Demonstrates this class.
-	 *
-	 * @param args unused
-	 */
-	public static void main(String[] args) {
-		int threads = 3;
-
-		Supplier<String> activeThreads = () -> {
-			Thread[] found = new Thread[Thread.activeCount() * 2];
-			Thread.enumerate(found);
-			return Arrays.stream(found)
-					.filter(Objects::nonNull)
-					.map(Thread::getName)
-					.collect(Collectors.joining(", "));
-		};
-
-		// demonstrates the workers in the background
-		WorkQueue demo = new WorkQueue(threads);
-
-		// do a bit of work in the background
-		demo.execute(() -> {
-			try {
-				Thread.sleep(500);
-			}
-			catch (InterruptedException e) {
-				Thread.currentThread().interrupt();
-			}
-		});
-
-		// finish, but do not shutdown
-		demo.finish();
-
-		// peek at the threads active in the system
-		System.out.println("Estimated active threads before join(): " + activeThreads.get());
-
-		// trigger threads to shutdown
-		demo.join();
-
-		// peek at the threads after shutdown and join
-		System.out.println("Estimated active threads after join(): " + activeThreads.get());
-
-		/*
-		 * The thread named "main" runs the main method.
-		 *
-		 * If you see any thread names starting with "ForkJoinPool", those are used
-		 * internally. They could come from Log4j2 or JUnit.
-		 *
-		 * If you see any thread names starting with "WorkerThread", those are
-		 * likely your work queue threads.
-		 *
-		 * If you see your work queue threads AFTER the join() call, something is
-		 * not quite working with the finish and/or shutdown calls.
-		 */
 	}
 }
